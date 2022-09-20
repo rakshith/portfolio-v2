8295f20c1cbc6a55c25b084f73578f915cf941ff
Rakshith <rakshith.raj@tigerspike.com>
2022-09-20 19:39:12 +0530

    form added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import About from "../components/About";
+import ContactMe from "../components/ContactMe";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
@@ -8,7 +9,10 @@ import WorkExperience from "../components/WorkExperience";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div
+      className="bg-[rgb(36,36,36)] text-white h-screen 
+    snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0"
+    >
       <Header />
 
       <section id="hero" className="snap-start">
@@ -31,6 +35,9 @@ const Home: NextPage = () => {
         <Projects />
       </section>
       {/* Contact Me */}
+      <section id="contact" className="snap-start">
+        <ContactMe />
+      </section>
     </div>
   );
 };
